aaf72aef641bacf4064a15efc95b0b63e31a433d
ackindev <ackinlino123@gmail.com>
2025-02-05 11:59:51 -0300

    Realizando Calculos Utilizando eval

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.addEventListener("click", (event) => {
-  console.log("Clicado");
+let buttons = document.querySelectorAll(".button");
+let display = document.getElementById("display");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    let buttonValue = button.value;
+
+    if (buttonValue === "AC") {
+      display.value = "";
+    } else if (buttonValue === "=") {
+      try {
+        display.value = eval(display.value);
+      } catch (e) {
+        display.value = "Erro";
+      }
+    } else {
+      display.value += buttonValue;
+    }
+  });
 });
